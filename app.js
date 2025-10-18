fef3bd2cae667772058abab12d725005cffbf0e7
LenyBl <blee.leny@gmail.com>
2025-10-18 10:24:32 +0200

    feat: connection to database

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const dbHost = process.env.DB_HOST 
+const mongoose = require('mongoose')
+
 
 app.get('/', (req, res) => {
     res.send('Hello World !')
@@ -9,3 +14,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Server started https://localhost:3000')
 })
+
+
+mongoose.connect(dbHost)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err))
+
+
+module.exports = app
+
+
