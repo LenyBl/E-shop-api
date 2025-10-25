2a004fdac5623c833496c2608b49983c2a5f9212
LenyBl <blee.leny@gmail.com>
2025-10-25 23:01:59 +0200

    feat: Helmet, xssMiddleware, cors and limiter request

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const helmet = require('helmet')
+const cors = require('cors')
 const connectDB = require('./config/database')
+const rateLimit = require('express-rate-limit')
+const xssMiddleware = require('./middlewares/xssMiddleware')
+const port = process.env.PORT || 3000
 
 const productRoutes = require('./routes/ProductRoute')
 const userRoutes = require('./routes/UserRoute')
 const cartRoutes = require('./routes/CartRoutes')
 const authRoutes = require('./routes/authRoutes')
 
+const app = express()
+
 app.use(express.json())
+
+app.use(helmet())
+
+app.use(xssMiddleware)
+
+app.use(cors(
+    {
+        origin: ['http://localhost:3000', 'https://yourdomain.com'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true
+    }
+))
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again later.'
+})
+app.use(limiter)
 
 app.get('/', (req, res) => {
     res.send('Hello World !')
@@ -21,7 +46,7 @@ app.use('/api/carts', cartRoutes)
 app.use('/api/auth', authRoutes)
 
 app.listen(port, () => {
-    console.log(`Server started on https://localhost:${port}`)
+    console.log(`Server started on http://localhost:${port}`)
 })
 
 connectDB();
